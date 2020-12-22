09bb1bbba96c84a800d5575e80cf9cc030d39567
Thomas Leese <thomas@leese.io>
2020-12-22 09:26:56 +0000

    Set locale to UK English

--- project/settings.py
+++ project/settings.py
@@ -99,18 +99,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+LANGUAGE_CODE = "en-gb"
+TIME_ZONE = "Europe/London"
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
