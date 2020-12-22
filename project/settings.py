c8de388ddf839b38902fcf797ec9e2b882b559d2
Thomas Leese <thomas@leese.io>
2020-12-22 10:20:59 +0000

    Don't set a secret key

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "m7jldzpde1k$lk!mixq#z-5s1s9t+5ad0bigqyjy=qb$ky38o4"
+SECRET_KEY = "CHANGE-ME!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
